35ea03146f3ba17c9e734b058d18a0b347bb2ba9
Alesson Evangelista <ealesson@gmail.com>
2023-12-21 13:33:58 -0300

    Configure RouteServiceProvider and create User Controller

---
+++ backend/routes/v1/User.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\api\v1\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(UserController::class)
+    ->group(function () {
+        Route::resource('', 'UserController')->except([
+                'create', 'edit',
+            ]);
+        // Route::get('/', 'index');
+    });
